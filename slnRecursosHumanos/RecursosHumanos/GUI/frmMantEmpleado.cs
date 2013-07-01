d2b1bc47d212ee921ff6a9ed99292ee551a0295d
Bryan Chaves <bryanchaves.salas19@gmailcom>
2013-07-01 17:07:35 -0600

    Actualización de la parte de Mantenimiento de Empleado

--- slnRecursosHumanos/RecursosHumanos/GUI/frmMantEmpleado.cs
+++ slnRecursosHumanos/RecursosHumanos/GUI/frmMantEmpleado.cs
@@ -14,15 +14,24 @@ namespace GUI
 {
     public partial class frmMantEmpleado : Form
     {
+        /// <summary>
+        /// Atributos de la clase Empleado  el cual recibe en su parte lógica un solo parámetro el cual es la conexión con Oracle
+        /// </summary>
         AccesoDatosOracle cnx;
 
+        /// <summary>
+        /// Método Constructor de la parte grafica Empleado el cual recibe la conexión a la base de datos Oraclr 
+        /// e inicializa los atributos con el parámetro que recibe y además llama al método cargar grid
+        /// </summary> 
         public frmMantEmpleado(AccesoDatosOracle pConexion)
         {
             InitializeComponent();
             this.cnx = pConexion;
             this.cargarGrid();
         }
-
+        /// <summary>
+        /// Método que se encarga de establecer la conexión con la base de datos adémas muestra los datos de esta en el grid
+        /// /// </summary> 
         public void cargarGrid()
         {
             try
@@ -39,7 +48,11 @@ namespace GUI
                 MessageBox.Show("Error cargando los datos" + e.Message);
             }
         }
-
+        /// <summary>
+        /// Método que se encarga de crear un objeto de edición empleado el cual biene cargado con los datos suministrados por el usuario
+        ///luego establece conexión con la clase datos de Empleado y cuadno la establece crea un objeto en la clase lógica de  empleado
+        ///
+        /// /// </summary> 
         private void btnNuevo_Click_1(object sender, EventArgs e)
         {
 
@@ -64,7 +77,10 @@ namespace GUI
             }
                        
         }
-
+        /// <summary>
+        /// Método que se encarga de editar un Empleado ya existente y procede de la siguiente manera toma el empleado existente y lo modifica por el suministrado 
+        ///
+        /// /// </summary> 
         private void btnEditar_Click(object sender, EventArgs e)
         {
 
@@ -92,7 +108,10 @@ namespace GUI
             }
 
         }
-
+        /// <summary>
+        /// Método que se encarga de borrar un Empleado ya existe primero consulta al usuario si desea elinarlo y si esta de acuerdo procede a establecer
+        /// la conexión con la base de datos y borra ese elemento
+        /// /// </summary> 
         private void btnBorrar_Click(object sender, EventArgs e)
         {
             if (this.grdEmpleado.RowCount > 0)
@@ -116,7 +135,9 @@ namespace GUI
                 }
             }
         }
-
+        /// <summary>
+        /// Método que se encarga de actualizar los datos de los empleados existentes
+        /// /// </summary>
         private void btnRefrescar_Click(object sender, EventArgs e)
         {
             this.cargarGrid();
