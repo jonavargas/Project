be4d99c76e9905bb4add454c52d1acd60b04f052
Ericka Jimenez Barquero <erickajb88@gmail.com>
2013-07-02 10:56:19 -0600

    cambios

--- slnRecursosHumanos/RecursosHumanos/GUI/frmEdicionEmpleado.cs
+++ slnRecursosHumanos/RecursosHumanos/GUI/frmEdicionEmpleado.cs
@@ -108,9 +108,6 @@ namespace GUI
             this.Close();
         }
 
-        private void btnCancelar_Click_1(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+       
     }
 }
