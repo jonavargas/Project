93a4bf45528b4ff1d41581bc44e31773c2351ad3
jonavargas <jonavargas2012@gmail.com>
2013-07-24 14:47:20 -0600

    Combos autocarbles consulta marca

--- slnRecursosHumanos/RecursosHumanos/GUI/frmConsultaMarcas.cs
+++ slnRecursosHumanos/RecursosHumanos/GUI/frmConsultaMarcas.cs
@@ -22,10 +22,40 @@ namespace GUI
         public frmConsultaMarcas(AccesoDatosOracle pConexion)
         {
             InitializeComponent(); 
-            InitializeComponent();
             this.conexion = pConexion;
             this.dteFecha1.Value = DateTime.Today;
             this.dteFecha2.Value = DateTime.Today.AddHours(23).AddMinutes(59).AddSeconds(59);
+            this.cargarCmbDepartamento(pConexion);
+            this.cargarComboEmpleado(pConexion);
+            this.cargarComboCodigoEmpleado(pConexion);
+            this.cmbEstado.Items.Add("Generada");
+            this.cmbEstado.Items.Add("En trámite");
+            this.cmbEstado.Items.Add("Pagada");
+            this.cmbEstado.Items.Add("Anulada");
+        }
+
+        public void cargarCmbDepartamento(AccesoDatosOracle pcnx)
+        {
+            DepartamentoD oDepartamentoD = new DepartamentoD(pcnx);
+            cmbDepartamento.DataSource = oDepartamentoD.obtenerIdDepartamento().Tables[0].Copy();
+            cmbDepartamento.DisplayMember = "idDepartamento";
+            cmbDepartamento.ValueMember = "idDepartamento";
+        }
+
+        public void cargarComboEmpleado(AccesoDatosOracle pcnx)
+        {
+            EmpleadoD oEmpleadoD = new EmpleadoD(pcnx);
+            cmbEmpleado.DataSource = oEmpleadoD.obtenerNombreEmpleado().Tables[0].Copy();
+            cmbEmpleado.DisplayMember = "nombreCompleto";
+            cmbEmpleado.ValueMember = "nombreCompleto";
+        }
+
+        public void cargarComboCodigoEmpleado(AccesoDatosOracle pcnx)
+        {
+            EmpleadoD oEmpleadoD = new EmpleadoD(pcnx);
+            cmbCodigo.DataSource = oEmpleadoD.obtenerCodigoEmpleado().Tables[0].Copy();
+            cmbCodigo.DisplayMember = "idEmpleado";
+            cmbCodigo.ValueMember = "idEmpleado";
         }
 
         private void btnFiltrar_Click(object sender, EventArgs e)
